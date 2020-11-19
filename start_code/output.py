ec0a68174ba53a6aa0cf1e4034029d33f76e2030
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-19 15:39:07 +0000

    first 3 done

---
+++ start_code/output.py
@@ -0,0 +1,21 @@
+def mark_task_complete(task):
+    task["completed"] = True
+
+# Extensions
+
+# create a task
+
+
+def create_task(description, time_taken):
+    task = {}
+    task["description"] = description
+    task["completed"] = False
+    task["time_taken"] = time_taken
+
+    return task
+
+# add a task to the list
+
+
+def add_to_list(list, task):
+    list.append(task)
